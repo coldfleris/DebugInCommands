21a2d49fa2bf9df6df401e0baecac104aa131e22
coldfleris <114491687+coldfleris@users.noreply.github.com>
2022-10-23 17:11:30 -0400

    add aliases, loggers and cleanup code (also blank lang json)

--- src/main/java/gov/coldfleris/debugincommands/Main.java
+++ src/main/java/gov/coldfleris/debugincommands/Main.java
@@ -1,10 +1,16 @@
 package gov.coldfleris.debugincommands;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /* Please note that keybinds are NOT implemented into this mod. This is intended to be used in conjunction with Wynncraft for 1.18.2, and feel free to use Wynntils (Project Artemis) to implement keybinds using the Register Keybinds feature. */
 
 public class Main implements ModInitializer {
+
+	public static final String MOD_ID = "debugincommands";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
 
 	@Override
--- src/main/java/gov/coldfleris/debugincommands/commands/ToggleHitboxesCommand.java
+++ src/main/java/gov/coldfleris/debugincommands/commands/ToggleHitboxesCommand.java
@@ -2,23 +2,28 @@ package gov.coldfleris.debugincommands.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
+import gov.coldfleris.debugincommands.Main;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.TranslatableText;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
-@SuppressWarnings("ALL")
+
 public class ToggleHitboxesCommand {
-
-    private static MinecraftClient mc = null;
-
-    public ToggleHitboxesCommand() {
-
-    }
-
-
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("togglehitboxes")
+                .executes(ToggleHitboxesCommand::toggleTheHitboxes)
+        );
+
+        //literal("hitboxes") is an alias to parent togglehitboxes
+        dispatcher.register(literal("hitboxes")
+                .executes(ToggleHitboxesCommand::toggleTheHitboxes)
+        );
+
+        //literal("th") is an alias to parent togglehitboxes
+        dispatcher.register(literal("th")
                 .executes(ToggleHitboxesCommand::toggleTheHitboxes)
         );
 
@@ -27,6 +32,15 @@ public class ToggleHitboxesCommand {
     public static int toggleTheHitboxes(CommandContext<ServerCommandSource> serverCommandSourceCommandContext) {
         boolean renderHitboxes = MinecraftClient.getInstance().getEntityRenderDispatcher().shouldRenderHitboxes();
         MinecraftClient.getInstance().getEntityRenderDispatcher().setRenderHitboxes (!renderHitboxes);
+        ChatHud chatHud = MinecraftClient.getInstance().inGameHud.getChatHud();
+
+        //Announce in chat that the command was executed and the feature has been toggled.
+        chatHud.addMessage(new TranslatableText(renderHitboxes ? "debug.show_hitboxes.off" : "debug.show_hitboxes.on"));
+
+        //Note it in log - NOTE THAT THE WARN IS INTENDED BEHAVIOR.
+        Main.LOGGER.warn("Take note: User has executed a debug command!");
+
+
         return 0;
     }
 
